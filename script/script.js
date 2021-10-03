b25c3626affbfd31377cc5785370188c9d57fc06
kilokid <arlomov007@gmail.com>
2021-10-03 04:15:06 +0300

    finish watch lesson 12 witch ticher

---
+++ script/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+
+const todoData = [];
+
+const render = function() {
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+        '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+        '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        const btnTodoComplete = li.querySelector('.todo-complete');
+
+        btnTodoComplete.addEventListener('click', function() {
+            item.completed = !item.completed;
+            render();
+        });
+
+    });
+};
+
+todoControl.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+
+    todoData.push(newTodo);
+
+    render();
+});
+
+render();
